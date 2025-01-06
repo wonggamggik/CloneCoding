d3129d7407a06e0c9f112cd949571df1d8360b0e
wonjongin <wonggamggik@gmail.com>
2025-01-06 11:44:13 +0900

    Delete react_tech_study/official_react_documents-express_ui directory

--- react_tech_study/official_react_documents-express_ui/src/forward_props_to_component/deliver_child_jsx1.jsx
+++
@@ -1,3 +0,0 @@
-export default function Avatar({ person, size }) {
-  return <img src={person.url} alt={person.name} width={size} />;
-}
--- react_tech_study/official_react_documents-express_ui/src/importing_exporting_components/method_import_export_components_1.jsx
+++
@@ -1,5 +0,0 @@
-import Gallery from "./method_import_export_components_2";
-
-export default function App() {
-  return <Gallery />;
-}
